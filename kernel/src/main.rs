fbc68d003d4d5eb06f33d7c530accad92683c5ae
coder-cc <coder-cc@outlook.com>
2025-08-01 21:48:57 +0800

    feat: update workspace configuration and dependencies
    - Set resolver to "3" in Cargo.toml for improved dependency resolution.
    - Adjust release profile optimization level to "z" for smaller binaries.
    - Remove outdated Cargo.lock file to regenerate with updated dependencies.
    - Change bootloader dependency to bootloader_api version "0.11" in kernel's Cargo.toml.
    - Comment out bootloader entry point and main function in kernel's main.rs for future implementation.
    - Add a new linker script (link.ld) to define memory layout for the kernel.
    - Create a run script (run.sh) to streamline the build and execution process using QEMU.
    - Update target specification (x86_64-kc_os.json) to specify linker and panic strategy.

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,23 +1,21 @@
 #![no_std]
 #![no_main]
 
-use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use volatile::Volatile;
 
-entry_point!(kernel_main);
+// entry_point!(kernel_main, config = &BOOTLOADER_CONFIG);
 
-fn kernel_main(_boot_info: &'static BootInfo) -> ! {
-    let vga_buffer = 0xb8000 as *mut Volatile<u16>;
+// fn kernel_main(_boot_info: &'static BootInfo) -> ! {
+//     let vga_buffer = 0xb8000 as *mut Volatile<u16>;
 
-    for (i, byte) in b"Hello, OS in Rust!".iter().enumerate() {
-        unsafe {
-            (*vga_buffer.add(i)).write(0x0f00 | *byte as u16);
-        }
-    }
+//     for (i, byte) in b"Hello, OS in Rust!".iter().enumerate() {
+//         unsafe {
+//             (*vga_buffer.add(i)).write(0x0f00 | *byte as u16);
+//         }
+//     }
 
-    loop {}
-}
+//     loop {}
+// }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
